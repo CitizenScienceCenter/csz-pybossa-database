4e084aa163d74a17460a1e92383ecf722347a581
Daniel Böhler <daniel.boehler@uzh.ch>
2023-03-20 17:00:24 +1100

    added detailed log output for init script

--- initdb/init-user-db.sh
+++ initdb/init-user-db.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-echo "creating db pybossa with user pybossa and pw  $(cat ${PYBOSSA_PASSWORD_FILE})"
-
+echo "pybossa needs a seperated postgres database and user"
+echo "initializing pybossa database and user..."
 psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
 	CREATE USER pybossa WITH CREATEDB NOSUPERUSER PASSWORD '$(cat ${PYBOSSA_PASSWORD_FILE})';
 	CREATE DATABASE pybossa WITH OWNER pybossa;
 EOSQL
-echo "finished creating db"
+echo "initialization done"
