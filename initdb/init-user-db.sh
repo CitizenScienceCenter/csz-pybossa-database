53bd9ffda8290123e7af6125be1be7d5a86f3565
Daniel Böhler <daniel.boehler@uzh.ch>
2023-04-08 17:20:50 +1000

    prepared replication

--- initdb/init-user-db.sh
+++ initdb/init-user-db.sh
@@ -7,4 +7,11 @@ psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" 
 	CREATE USER pybossa WITH CREATEDB NOSUPERUSER PASSWORD '$(cat ${PYBOSSA_PASSWORD_FILE})';
 	CREATE DATABASE pybossa WITH OWNER pybossa;
 EOSQL
-echo "initialization done"
+echo "...done"
+
+# psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
+# 	CREATE USER replication WITH REPLICATION PASSWORD 'testpassword';
+# EOSQL
+
+#'$(cat ${PYBOSSA_PASSWORD_FILE})'
+
