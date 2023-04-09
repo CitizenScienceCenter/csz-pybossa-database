374c29f6dea60d54a895ec5f6a8a708b3ec2e744
Daniel Böhler <daniel.boehler@uzh.ch>
2023-04-09 19:24:00 +1000

    fixed backup to restorable dump

--- backup-db.sh
+++ backup-db.sh
@@ -4,20 +4,19 @@
 # backup postgresql database into a daily file.
 #
 
-# directory and path
-BACKUP_DIR=./pgbackup
-FILENAME=pybossa_pg_backup_
+# directory and path inside docker container
+BACKUP_DIR=/var/lib/postgresql/backup
+FILENAME=pybossa_pgbackup_
 SUFFIX=.sql
-
-# create dir if does not exist
-mkdir -p ${BACKUP_DIR}
 
 # file name
 OUTPUT_PATH=${BACKUP_DIR}/${FILENAME}`date +"%Y%m%d"`${SUFFIX}
 
 # create dump of database inside docker container with env variables of container
-docker exec postgres /bin/bash -c 'pg_dump -U pybossa pybossa' > ${OUTPUT_PATH}
+docker exec -i postgres pg_dump --clean --dbname=pybossa --username=postgres --file=${OUTPUT_PATH}
 
-# show the user the result
+# restore database with specific file
+#docker exec -i postgres /bin/bash -c "PGPASSWORD=<postgres-pw> psql --host=130.60.24.55 --username=pybossa --dbname=pybossa --file=pybossa-db.bak"
+
+# show result
 echo "${OUTPUT_PATH} was created:"
-ls -l ${OUTPUT_PATH}
