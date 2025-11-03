2ad6959f611994e15feabdbbe9530eabb94f881d
Daniel Böhler <daniel.boehler@uzh.ch>
2025-11-03 13:48:23 +0100

    Adapted verbose script text

--- initdb/init-user-db.sh
+++ initdb/init-user-db.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 set -e
-
-echo "pybossa needs a seperated postgres database and user"
-echo "initializing pybossa database and user..."
+printf "\npybossa: waiting for creation of database and user...\n"
 psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
 	CREATE USER pybossa WITH CREATEDB NOSUPERUSER PASSWORD '$(cat ${PYBOSSA_PASSWORD_FILE})';
 	CREATE DATABASE pybossa WITH OWNER pybossa TEMPLATE template0;
 EOSQL
-echo "...done"
+printf " done\n"
 
 # psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" --dbname "${POSTGRES_DB}" <<-EOSQL
 # 	CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'testpassword' LOGIN;
